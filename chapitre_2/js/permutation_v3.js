42daff43ac9ba4b648642feea09c501d0a180f4a
Morgan RO <moganro@gmail.com>
2018-04-23 17:15:51 +0200

    Proposing solution with increments
    Hi, I just wanted to propose "in/de - crement" , another solution using the course. 
    Cheers and thanks for your course !
    
    Morgan

---
+++ chapitre_2/js/permutation_v3.js
@@ -0,0 +1,9 @@
+var nombre1 = 5;
+var nombre2 = 3;
+
+// my proposal, using other knowledges from the course
+nombre1 -= 2
+nombre2 += 2
+
+console.log(nombre1); // Doit afficher 3
+console.log(nombre2); // Doit afficher 5
