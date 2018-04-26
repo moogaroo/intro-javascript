be177a1352d15712fa98e6227baf8151253d511d
Morgan Rose <moganro@gmail.com>
2018-04-26 18:58:23 +0200

    Updating bac.js with prompt errors
    Avec le code précédent, le problème est qu'en tapant une moyenne <0 ou >20 ou un string, la console renvoie "recalé" ou "reçu avec mention" .
    Je propose donc un code qui intègre l'erreur de saisie.

--- chapitre_3/js/bac.js
+++ chapitre_3/js/bac.js
@@ -2,12 +2,14 @@
 Exercice : trouver le résultat au bac selon la moyenne
 */
 
-var moyenne = Number(prompt("Entrez une moyenne de baccalauréat :"));
+var moyenne = Number(prompt("Indiquer ici la moyenne"));
 
-if (moyenne < 10) {
-    console.log("Le candidat est recalé");
-} else if (moyenne < 12) { // 10 <= moyenne < 12
-    console.log("Le candidat est reçu");
-} else { // moyenne >= 12
-    console.log("Le candidat est reçu avec mention");
+if ((moyenne < 10) && (moyenne >= 0 )) { //  0 <= moyenne < 10
+        alert("le candidat est recalé");   
+} else if ((moyenne >= 10) && (moyenne < 12))  { // 10 <= moyenne < 12
+        alert("le candidat est reçu");
+} else if ((moyenne >= 12)  && (moyenne <= 20)) { //  12 <= moyenne <= 20
+        alert("le candidat est reçu avec mention");
+} else { // ((moyenne < 0)  || (moyenne > 20) || (moyenne != Number))
+        alert("Désolé, la note doit être un chiffre compris en 0 et 20");
 }
